bad09616fe1659ee8b124e6bd0f7623dee37f7c5
Firman <fblazt@outlook.com>
2021-02-23 22:29:00 +0700

    Rework ratings response

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,7 +6,7 @@ export default function RestaurantCard({ restaurantInfo, restaurantDetail, revie
     let ratings = []
     for (let i = 0; i < length; i++) {
       ratings.push(
-        <svg key={i} className="h-4 w-4 text-yellow-300" viewBox="0 0 24 24">
+        <svg key={i} className="h-5 w-5 text-yellow-500" viewBox="0 0 24 24">
           <path fill="currentColor" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
         </svg>
       )
@@ -24,21 +24,7 @@ export default function RestaurantCard({ restaurantInfo, restaurantDetail, revie
       <img src={restaurantInfo.pict} alt="" className="mb-4 rounded-3xl shadow-lg"/>
       <h4 className="mb-3 text-2xl font-semibold">{restaurantInfo.name}</h4>
       <div className="mb-1 flex flex-nowrap text-yellow-500">
-        <svg className="h-5 w-5" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
-        </svg>
-        <svg className="h-5 w-5" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
-        </svg>
-        <svg className="h-5 w-5" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
-        </svg>
-        <svg className="h-5 w-5" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
-        </svg>
-        <svg className="h-5 w-5" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
-        </svg>
+        {overallRatings(restaurantInfo.rating)}
       </div>
       <p className="mb-4 text-sm">{restaurantInfo.address}</p>
       <button onClick={review} className="py-2 px-4 bg-blue-500 transition hover:bg-blue-600 text-white rounded-full shadow-md">Add review +</button>
